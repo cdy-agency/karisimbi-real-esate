81306177994b5fa180dc1087a50e40d20b378719
Mucyo Papy Blaise <mucyoblaise86@gmail.com>
2026-06-20 16:38:09 +0200

    remove stats section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <div>
       <Hero />
-      <StatsSection />
+      {/* <StatsSection /> */}
       <PropertiesSection />
       <LatestProjects />
       <ContactCta />
