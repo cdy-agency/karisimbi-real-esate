a07f61e37f149452f9f162adfa7f3f2e8c1d226d
Mucyo Papy Blaise <mucyoblaise86@gmail.com>
2026-06-20 16:46:51 +0200

    quick changes new ones

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,9 +8,9 @@ export default function Home() {
   return (
     <div>
       <Hero />
-      <StatsSection />
       <PropertiesSection />
       <LatestProjects />
+      <StatsSection />
       <ContactCta />
     </div>
   );
